bd2f7ecce8ceb94941d04d853fc1a92f32361c48
arthurstrokov <arthurstrokov@gmail.com>
2019-05-07 14:25:43 +0300

    up add migration add films

---
+++ src/main/resources/db/migration/V3__Add_film.sql
@@ -0,0 +1,5 @@
+insert into film (user_id, genre, title, year, id)
+values (1, 'Superhero comedy-drama', 'Hancock', '2008', 1);
+
+insert into film (user_id, genre, title, year, id)
+values (1, 'Anime', 'Dragon Ball Z', 'April 26, 1989 – January 31, 1996', 2)
